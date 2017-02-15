0c74c4e884e1574eabdd405ee353d358a6f7d7e8
Rahul Gupta <useritch96@gmail.com>
2017-02-15 07:58:27 +0530

    Added the fullPage functionality

---
+++ Projects/projectApp.js
@@ -0,0 +1,46 @@
+var main = function(){
+    $('#fullpage').fullpage({
+        //Scrolling
+        css3: true,
+        scrollingSpeed: 700,
+        autoScrolling: true,
+        fitToSection: true,
+        fitToSectionDelay: 1000,
+        scrollBar: false,
+        easing: 'easeInOutCubic',
+        easingcss3: 'ease',
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: true,
+        continuousVertical: true,
+        continuousHorizontal: false,
+        scrollHorizontally: false,
+        interlockedSlides: false,
+        dragAndMove: false,
+        offsetSections: false,
+        resetSliders: false,
+        fadingEffect: true,
+        // normalScrollElements: '#element1, .element2',
+        scrollOverflow: false,
+        scrollOverflowReset: false,
+        scrollOverflowOptions: null,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+
+        //Accessibility
+        keyboardScrolling: true,
+        animateAnchor: true,
+        recordHistory: true,
+        verticalCentered: false,
+        lazyLoading: true,
+    });
+    $('.prev-icon').click(function(){
+        $.fn.fullpage.moveSectionUp();
+    });
+
+    $('.next-icon').click(function(){
+        $.fn.fullpage.moveSectionDown();
+    });
+};
+
+$(document).ready(main);
